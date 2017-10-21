100bc866a1c2285817003a2abf5cb9052df3107e
Kimmoonsu <visitant2@naver.com>
2017-10-21 21:38:29 +0900

    Enable to disConnect.

--- src/main/java/com/skuniv/QuickPollSocketServer/Student/StudentSocketService.java
+++ src/main/java/com/skuniv/QuickPollSocketServer/Student/StudentSocketService.java
@@ -15,23 +15,31 @@ import com.google.gson.GsonBuilder;
 import com.skuniv.QuickPollSocketServer.LectureMember;
 import com.skuniv.QuickPollSocketServer.LectureModel;
 import com.skuniv.QuickPollSocketServer.MessageVO;
+import com.skuniv.QuickPollSocketServer.model.LectureVO;
 
 @Service("StudentSocketService")
 public class StudentSocketService {
 	private LectureModel lectureModel = new LectureModel().getIntstance();
 	private Map<String, LinkedHashMap<WebSocketSession, LectureMember>> memberList = lectureModel.getMemberList();
-	public void enterLecture(WebSocketSession session, TextMessage message, MessageVO messageVO) {
-		if (isLecture(messageVO)) {
+	public boolean enterLecture(WebSocketSession session, TextMessage message, MessageVO messageVO) {
+		if (isLecture(messageVO.getCourse_id())) {
 			LinkedHashMap<WebSocketSession, LectureMember> map = lectureModel.getLectureMap(messageVO);
     		map.put(session, new LectureMember(messageVO.getName(), messageVO.getId(), session));
     		memberList.put(messageVO.getCourse_id(), map);
+    		return true;
 		} else {
+			return false;
 			// can not connect. not exist lecture
 		}
 	}
-	public boolean isLecture(MessageVO messageVO) {
-		if (memberList.containsKey(messageVO.getCourse_id())) return true;
+	public boolean isLecture(String course_id) {
+		if (memberList.containsKey(course_id)) return true;
 		else return false;
+	}
+	public LectureVO setState(boolean checkState) {
+		LectureVO lectureVO = new LectureVO();
+		lectureVO.setCheckState(checkState);
+		return lectureVO;
 	}
 	// send to directquestion answer to professor from student.
 	public void sendDirectQuestionAnwserToProfessor(MessageVO messageVO) throws IOException {
